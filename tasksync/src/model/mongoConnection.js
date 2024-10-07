fd0ac80cff7269f3daec092f60d30171e62b1186
Jason Appiah <jasona2@umbc.edu>
2024-10-07 16:50:50 -0400

    begining of database implementation

---
+++ tasksync/src/model/mongoConnection.js
@@ -0,0 +1,18 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Replace the following with your MongoDB connection string.
+const uri = "mongodb+srv://jasona2:8Ea0i9A1OqbvQ1ZR@schedulercluster.gowe7.mongodb.net/?retryWrites=true&w=majority&appName=SchedulerCluster";;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    return client.db('your-database-name'); // Replace with your database name
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+module.exports = connectToDatabase;
