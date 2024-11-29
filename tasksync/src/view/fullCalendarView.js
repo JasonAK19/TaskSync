27abe831aa905554b5f040dcdb91048176ed04b0
Jason Appiah <jasona2@umbc.edu>
2024-11-29 15:43:51 -0500

    personal task have a more in depth view on full calendar, on to display group tasks

--- tasksync/src/view/fullCalendarView.js
+++ tasksync/src/view/fullCalendarView.js
@@ -6,7 +6,6 @@ import axios from 'axios';
 import './fullCalendarView.css';
 import Header from './components/header';
 import Sidebar from './components/sidebar';
-
 const FullCalendarView = ({ username, userInfo, onLogout, groups, setGroups }) => {
   const [tasks, setTasks] = useState([]);
   const [groupTasks, setGroupTasks] = useState([]);
@@ -27,13 +26,51 @@ const FullCalendarView = ({ username, userInfo, onLogout, groups, setGroups }) =
     fetchTasks();
   }, [username]);
 
-  const allTasks = [...tasks, ...groupTasks].map(task => ({
-    id: task._id,
-    title: task.title,
-    start: task.startDateTime,
-    end: task.endDateTime,
-    className: 'task-event'
-  }));
+  const taskEvents = tasks.map(task => {
+    const taskDate = task.date;
+    // Use saved time if it exists, otherwise default to 9:00
+    const startDateTime = task.time ? 
+      `${taskDate}T${task.time}:00` :
+      `${taskDate}T09:00:00`;
+      
+    // Set end time 1 hour after start
+    const [hours, minutes] = (task.time || '09:00').split(':');
+    const endHour = String(Number(hours) + 1).padStart(2, '0');
+    const endDateTime = `${taskDate}T${endHour}:${minutes}:00`;
+  
+    return {
+      id: task._id,
+      title: task.title,
+      start: startDateTime,
+      end: endDateTime,
+      className: 'task-event'
+    };
+  });
+  
+  const groupTaskEvents = groupTasks.map(task => {
+    const taskDate = task.date;
+    const startDateTime = `${taskDate}T09:00:00`;
+    const endDateTime = `${taskDate}T10:00:00`;
+  
+    return {
+      id: task._id,
+      title: task.title,
+      start: startDateTime,
+      end: endDateTime,
+      className: 'group-task-event'
+    };
+  });
+
+  const allEvents = [...taskEvents, ...groupTaskEvents];
+
+  const eventContent = (arg) => {
+    return (
+      <div className={`custom-event ${arg.event.classNames}`}>
+        <span className="event-dot"></span>
+        <span className="event-title">{arg.event.title}</span>
+      </div>
+    );
+  };
 
   return (
     <div className="full-calendar-page">
@@ -44,7 +81,7 @@ const FullCalendarView = ({ username, userInfo, onLogout, groups, setGroups }) =
           <FullCalendar
             plugins={[dayGridPlugin, timeGridPlugin]}
             initialView="timeGridWeek"
-            events={allTasks}
+            events={allEvents}
             headerToolbar={{
               left: 'prev,next today',
               center: 'title',
@@ -53,6 +90,7 @@ const FullCalendarView = ({ username, userInfo, onLogout, groups, setGroups }) =
             height="100%"
             contentHeight="100%"
             eventDisplay="block"
+            eventContent={eventContent}
           />
         </div>
       </div>
